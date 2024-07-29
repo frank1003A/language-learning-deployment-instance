96a5462c819a2b45775518678760893491a16f84
atonya-bravin <bravinatonya.brani@gmail.com>
2024-07-29 19:21:38 +0300

    Creation or the preview buttons.
    This creates preview button components that can be used within other
    pages.

---
+++ src/components/preview-buttons/Loaded.js
@@ -0,0 +1,15 @@
+export function LoadedPreButton() {
+    return(
+        <div className="w-[68px] h-[68px] md:w-[84px] md:h-[84px] rounded-full bg-success-70 p-[6px] flex items-center justify-center ">
+            <div className="w-[56px] h-[56px] md:w-[72px] md:h-[72px] rounded-full bg-success-100 p-[4px] flex items-center justify-center">
+                <div className="w-[48px] h-[48px] md:w-[64px] md:h-[64px] rounded-full bg-success-120 p-[4px] flex items-center justify-center">
+                    <div className="w-[40px] h-[40px] md:w-[56px] md:h-[56px] rounded-full bg-transparent-black-75 p-[10px] flex items-center justify-center">
+                        <svg className="w-[20px] h-[20px] md:w-[32px] h-[32px]" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M26.6666 8L11.9999 22.6667L5.33325 16" stroke="white" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
