bd3080ce87ece5f205247800f1cdfa5f713ab0f2
theblvckdev <nathancodes05@gmail.com>
2024-07-29 18:00:07 +0100

    code update

--- src/app/(landing-routes)/(home)/page.tsx
+++ src/app/(landing-routes)/(home)/page.tsx
@@ -1,15 +1,7 @@
 function page() {
   return (
-<<<<<<< HEAD
-    <div className="">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe
-      excepturi, laudantium sapiente, voluptate repellat rem odit exercitationem
-      quam mollitia eaque ducimus tenetur dolorum dignissimos! Nesciunt
-      provident tempora laboriosam dolores.
-=======
     <div className="font-lilita">
       Welcome to our Language Learning platform.
->>>>>>> bc8c56c7e27a692b4800a0a76ca551fd03440bf4
     </div>
   );
 }
