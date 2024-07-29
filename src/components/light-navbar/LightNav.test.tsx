5778cfd67bbb7757cbf27c87e2976007cbe26b36
Angel Afube <angel@zestpayment.com>
2024-07-29 21:10:57 +0100

    merge dev and add new test file

---
+++ src/components/light-navbar/LightNav.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import LightNav from "./LightNav";
+
+import "@testing-library/jest-dom/extend-expect";
+
+describe("lightNav", () => {
+  it("renders desktop logo", () => {
+    expect.assertions(1); 
+    render(<LightNav />);
+    const desktopLogo = screen.getByAltText("desktop-logo");
+    expect(desktopLogo).toBeInTheDocument();
+  });
+
+  it("renders mobile logo", () => {
+    expect.assertions(1); 
+    render(<LightNav />);
+    const mobileLogo = screen.getByAltText("mobile-logo");
+    expect(mobileLogo).toBeInTheDocument();
+  });
+
+  it("renders Sign In and Sign Up buttons when not signed in", () => {
+    expect.assertions(2); // Adding this line to specify the number of assertions in the test
+    render(<LightNav />);
+    const signInButton = screen.getByText("Sign In");
+    const signUpButton = screen.getByText("Sign Up");
+    expect(signInButton).toBeInTheDocument();
+    expect(signUpButton).toBeInTheDocument();
+  });
+
+});
+
+
