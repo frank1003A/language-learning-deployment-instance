ba1ef7584acab92202357a30ab96a749746b32c2
owoadehabeeb04 <owoadehabeeb04@gmail.com>
2024-07-29 16:14:45 +0100

    feature/password-criteria

--- src/components/password/passwordCriteria.test.tsx
+++ src/components/password/passwordCriteria.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it, } from "vitest";
+import { describe, expect, it } from "vitest";
+
 import PasswordCriteria from "./passwordCriteria";
 
-describe('PasswordCriteria Component', () => {
-    it('should show all criteria as invalid when password is empty', () => {
-      render(<PasswordCriteria password="" />);
-      expect(screen.getByText(/8 Characters/i)).toBeInTheDocument();
-      expect(screen.getByText(/Uppercase Letter/i)).toBeInTheDocument();
-      expect(screen.getByText(/Lowercase Letter/i)).toBeInTheDocument();
-      expect(screen.getByText(/Special Character/i)).toBeInTheDocument();
-    });
+describe("passwordCriteriaComponent", () => {
+  it("should show all criteria as invalid when password is empty", () => {
+    render(<PasswordCriteria password="" />);
+    expect(screen.getByText(/8 Characters/i)).toBeInTheDocument();
+    expect(screen.getByText(/Uppercase Letter/i)).toBeInTheDocument();
+    expect(screen.getByText(/Lowercase Letter/i)).toBeInTheDocument();
+    expect(screen.getByText(/Special Character/i)).toBeInTheDocument();
   });
-  
+});
