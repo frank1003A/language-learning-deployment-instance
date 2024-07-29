87db336ec4f663b8ae7b64d285843275e2245b4f
KaiserX <wiz0isaac@gmail.com>
2024-07-29 16:31:54 +0100

    feat: add mission brief modal

---
+++ src/app/(landing-routes)/test-model/page.tsx
@@ -0,0 +1,10 @@
+import MissionBriefModal from "~/components/modals/MissionBriefModel";
+
+export default function TestModalPage() {
+  return (
+    <div>
+      <h1>Test Modal Page</h1>
+      <MissionBriefModal />
+    </div>
+  );
+}
