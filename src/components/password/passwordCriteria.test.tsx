3020ce9e06c52bfd4d02941a008ae057d53c004c
owoadehabeeb04 <owoadehabeeb04@gmail.com>
2024-07-29 16:19:03 +0100

    feature/password-criteria

--- src/components/password/passwordCriteria.test.tsx
+++ src/components/password/passwordCriteria.test.tsx
@@ -7,9 +7,9 @@ describe("passwordCriteriaComponent", () => {
   it("should show all criteria as invalid when password is empty", () => {
     expect.assertions(4);
     render(<PasswordCriteria password="" />);
-    expect(screen.getByText(/8 Characters/i)).toBeInTheDocument();
-    expect(screen.getByText(/Uppercase Letter/i)).toBeInTheDocument();
-    expect(screen.getByText(/Lowercase Letter/i)).toBeInTheDocument();
-    expect(screen.getByText(/Special Character/i)).toBeInTheDocument();
+    expect(screen.getByText(/8 characters/i)).toBeInTheDocument();
+    expect(screen.getByText(/uppercase letter/i)).toBeInTheDocument();
+    expect(screen.getByText(/lowercase letter/i)).toBeInTheDocument();
+    expect(screen.getByText(/special character/i)).toBeInTheDocument();
   });
 });
