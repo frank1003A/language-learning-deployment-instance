6cb79c9c74b99bd80c9e9da3f75c66fe667707f9
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-07-30 17:08:06 +0100

    create the accordion component

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -10,6 +10,7 @@ import { DefaultPreButton } from "~/components/preview-buttons/Default";
 import { DisabledPreButton } from "~/components/preview-buttons/Disabled";
 import { FocussedPreButton } from "~/components/preview-buttons/Focussed";
 import { LoadedPreButton } from "~/components/preview-buttons/Loaded";
+import Accordion from "~/components/ui/CustomAccordion";
 
 const StyleGuide: FC = () => {
   return (
@@ -731,6 +732,19 @@ const StyleGuide: FC = () => {
       {/* Modals */}
       <h2 className="text-2xl font-semibold">Modals</h2>
       <Cookies />
+
+      <Accordion
+        title="Accordion Heading"
+        content="These cookies are crucial for the website's basic functionality and cannot be disabled. They ensure that the website operates correctly and securely."
+        alwaysActive={false}
+        defaultOpen={false}
+      />
+      <Accordion
+        title="Accordion Heading"
+        content="These cookies are crucial for the website's basic functionality and cannot be disabled. They ensure that the website operates correctly and securely."
+        alwaysActive={true}
+        defaultOpen={true}
+      />
     </main>
   );
 };
