6c5c27ac7a2968bdd98b4fa6494ce1eaa2415572
Frank Ezene <ezenefrankc@gmail.com>
2024-07-30 07:20:26 +0100

    component added to page

--- src/app/(landing-routes)/(home)/page.tsx
+++ src/app/(landing-routes)/(home)/page.tsx
@@ -1,6 +1,12 @@
 import HeroSection from "~/components/HeroSection";
+import LearningGoalModal from "~/components/modals/LearningGoalModal";
 
 function page() {
-  return <HeroSection />;
+  return (
+    <div className="relative flex h-full w-full flex-col">
+      <HeroSection />
+      <LearningGoalModal />
+    </div>
+  );
 }
 export default page;
